09da076e6b6fea28e1944fa3beaabf21e3dd0a3a
VanciaC <vancia.chan@ipilyon.net>
2020-10-08 13:51:42 +0200

    Ex 2 - 206

--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -8,8 +8,21 @@ public class Commercial extends Employe{
 
     private Double caAnnuel = 0d;
 
+    private Integer performance;
+
     public Commercial(){
 
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+    }
+
+    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel, Integer performance) {
+        super(nom, prenom, matricule, dateEmbauche, salaire);
+        this.caAnnuel = caAnnuel;
+        this.performance = performance;
     }
 
     @Override
@@ -26,11 +39,6 @@ public class Commercial extends Employe{
         return Objects.hash(super.hashCode(), caAnnuel);
     }
 
-    public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {
-        super(nom, prenom, matricule, dateEmbauche, salaire);
-        this.caAnnuel = caAnnuel;
-    }
-
     public Commercial(Double caAnnuel) {
         this.caAnnuel = caAnnuel;
     }
@@ -39,6 +47,15 @@ public class Commercial extends Employe{
     @Override
     public Double getPrimeAnnuelle() {
         return Math.max(Math.ceil(this.getCaAnnuel() * 0.05), 500);
+    }
+
+    public boolean performanceEgale(Integer performance){
+        if(this.performance.equals(performance)){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public Double getCaAnnuel() {

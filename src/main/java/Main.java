f903098da692eb2fe26ef7e325077c00f1d91968
VanciaC <vancia.chan@ipilyon.net>
2020-10-08 13:45:41 +0200

    Ex 2 - 205

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
+import com.ipiecoles.java.java220.Commercial;
+import com.ipiecoles.java.java220.Employe;
+import org.joda.time.LocalDate;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args){
+        Employe employe1 = new Employe("Jules", "Bertrand", "135477", LocalDate.now(), 1480.20);
 
+        ArrayList<Employe> employes = new ArrayList<>();
+        employes.add(employe1);
+        employes.add(new Commercial());
+        for(Employe e : employes){
+            System.out.println(e);
+        }
     }
 }
--- src/main/java/com/ipiecoles/java/java220/Commercial.java
+++ src/main/java/com/ipiecoles/java/java220/Commercial.java
@@ -2,12 +2,28 @@ package com.ipiecoles.java.java220;
 
 import org.joda.time.LocalDate;
 
+import java.util.Objects;
+
 public class Commercial extends Employe{
 
     private Double caAnnuel = 0d;
 
     public Commercial(){
 
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Commercial that = (Commercial) o;
+        return Objects.equals(caAnnuel, that.caAnnuel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), caAnnuel);
     }
 
     public Commercial(String nom, String prenom, String matricule, LocalDate dateEmbauche, Double salaire, Double caAnnuel) {

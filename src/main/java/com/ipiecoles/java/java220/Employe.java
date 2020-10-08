61e6236eab1d93516d5b0ae1a3ba538b3f1b52e9
VanciaC <vancia.chan@ipilyon.net>
2020-10-08 10:53:05 +0200

    Ex 107 - 108

--- src/main/java/com/ipiecoles/java/java220/Employe.java
+++ src/main/java/com/ipiecoles/java/java220/Employe.java
@@ -2,20 +2,22 @@ package com.ipiecoles.java.java220;
 
 import org.joda.time.LocalDate;
 
+import java.util.Objects;
+
 /**
  * Created by pjvilloud on 21/09/17.
  */
 public class Employe {
 
-    private static String nom;
+    private String nom;
 
-    private static String prenom;
+    private String prenom;
 
-    private static String matricule;
+    private String matricule;
 
-    private static LocalDate dateEmbauche;
+    private LocalDate dateEmbauche;
 
-    private static Double salaire;
+    private Double salaire;
 
     public Employe() {
 
@@ -29,65 +31,82 @@ public class Employe {
         this.salaire = salaire;
     }
 
-    public  final Integer getNombreAnneeAnciennete(){
+    public final Integer getNombreAnneeAnciennete() {
         return LocalDate.now().getYear() - dateEmbauche.getYear();
     }
 
-    public Integer getNbConges(){
+    public Integer getNbConges() {
         return Entreprise.NB_CONGES_BASE;
     }
 
-    public String toString(){
-        final StringBuilder sb = new StringBuilder("Employe{");
-        sb.append("nom='").append(nom).append('\'');
-        sb.append(", prenom='").append(prenom).append('\'');
-        sb.append(", matricule='").append(matricule).append('\'');
-        sb.append(", dateEmbauche=").append(dateEmbauche);
-        sb.append(", salaire=").append(salaire);
-        sb.append('}');
-        return sb.toString();
+    @Override
+    public String toString() {
+        return "Employe{" +
+                "nom='" + nom + '\'' +
+                ", prenom='" + prenom + '\'' +
+                ", matricule='" + matricule + '\'' +
+                ", dateEmbauche=" + dateEmbauche +
+                ", salaire=" + salaire +
+                '}';
     }
 
-    public static String getNom() {
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employe employe = (Employe) o;
+        return Objects.equals(nom, employe.nom) &&
+                Objects.equals(prenom, employe.prenom) &&
+                Objects.equals(matricule, employe.matricule) &&
+                Objects.equals(dateEmbauche, employe.dateEmbauche) &&
+                Objects.equals(salaire, employe.salaire);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nom, prenom, matricule, dateEmbauche, salaire);
+    }
+
+    public String getNom() {
         return nom;
     }
 
-    public static String getPrenom() {
+    public String getPrenom() {
         return prenom;
     }
 
-    public static String getMatricule() {
+    public String getMatricule() {
         return matricule;
     }
 
-    public static LocalDate getDateEmbauche() {
+    public LocalDate getDateEmbauche() {
         return dateEmbauche;
     }
 
-    public static Double getSalaire() {
+    public Double getSalaire() {
         return salaire;
     }
 
-    public static void setNom(String nom) {
-        Employe.nom = nom;
+    public void setNom(String nom) {
+        this.nom = nom;
     }
 
-    public static void setPrenom(String prenom) {
-        Employe.prenom = prenom;
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
     }
 
-    public static void setMatricule(String matricule) {
-        Employe.matricule = matricule;
+    public void setMatricule(String matricule) {
+        this.matricule = matricule;
     }
 
-    public static void setDateEmbauche(LocalDate dateEmbauche) throws Exception {
+    public void setDateEmbauche(LocalDate dateEmbauche) throws Exception {
         if (dateEmbauche != null && dateEmbauche.isAfter(LocalDate.now())){
             throw new Exception("La date d'embauche ne peut être postérieure à la date courante");
         }
-        Employe.dateEmbauche = dateEmbauche;
+        this.dateEmbauche = dateEmbauche;
     }
 
-    public static void setSalaire(Double salaire) {
-        Employe.salaire = salaire;
+    public void setSalaire(Double salaire) {
+        this.salaire = salaire;
     }
 }
